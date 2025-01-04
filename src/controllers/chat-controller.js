52edfbaad4f0d216e191065cd287ce065d3d861b
Tushar2001bbdu <146729736+Tushar2001bbdu@users.noreply.github.com>
2025-01-04 14:28:42 +0530

    Update chat-controller.js

--- src/controllers/chat-controller.js
+++ src/controllers/chat-controller.js
@@ -1,74 +1,74 @@
 const Response = require("../utils/response");
 const chatService = require("../services/chat-service");
 
-exports.getRoomProfile = async (req, res) => {
+exports.getSquadProfile = async (req, res) => {
   try {
     const { roomId } = req.params;
-    const roomProfile = await chatService.fetchRoomProfile(roomId);
+    const squadProfile = await chatService.fetchSquadProfile(roomId);
 
-    res.json({status:true,message:"Room profile fetched successfully", data:roomProfile});
+    res.json({ status: true, message: "Squad profile fetched successfully", data: squadProfile });
   } catch (error) {
-    console.error("Error in getRoomProfile:", error.message);
+    console.error("Error in getSquadProfile:", error.message);
     return Response.error(res, error.message || "An error occurred");
   }
 };
 
-exports.joinChatRoom = async (req, res) => {
+exports.joinSquad = async (req, res) => {
   try {
     const { roomId, username } = req.body;
-    const response = await chatService.joinChatRoom(username, roomId);
+    const response = await chatService.joinSquad(username, roomId);
 
     if (response instanceof Error) {
       switch (response.message) {
         case "User not found":
           return Response.unauthorized(res, "User not found");
-        case "Room not found":
-          return Response.notFound(res, "Room not found");
-        case "User already added to the room":
-          return Response.badRequest(res, "User already added to the room");
-        case "Group administrator cannot join the group explicitly":
-          return Response.badRequest(res, "Group administrator cannot join the group explicitly");
+        case "Squad not found":
+          return Response.notFound(res, "Squad not found");
+        case "User already added to the squad":
+          return Response.badRequest(res, "User already added to the squad");
+        case "Group administrator cannot join the squad explicitly":
+          return Response.badRequest(res, "Group administrator cannot join the squad explicitly");
         default:
           return Response.error(res, "Unexpected error occurred");
       }
     }
 
-    return Response.success(res, "User has joined room successfully");
+    return Response.success(res, "User has joined squad successfully");
   } catch (error) {
-    console.error("Error in joinChatRoom:", error.message);
+    console.error("Error in joinSquad:", error.message);
     return Response.error(res, error.message || "Server error");
   }
 };
 
-exports.getAllChatRooms = async (req, res) => {
+exports.getAllSquads = async (req, res) => {
   try {
     const { username } = req.body;
-    const response = await chatService.getAllRooms(username);
+    const response = await chatService.getAllSquads(username);
 
     if (response instanceof Error && response.message === "User not found") {
       return Response.unauthorized(res, "User not found");
     }
 
-    res.json({status:true,message:"Chat rooms fetched successfully",data:response});
+    res.json({ status: true, message: "Squads fetched successfully", data: response });
   } catch (error) {
-    console.error("Error in getAllChatRooms:", error.message);
+    console.error("Error in getAllSquads:", error.message);
     return Response.error(res, error.message || "Server error");
   }
 };
 
-exports.leaveChatRoom = async (req, res) => {
+exports.leaveSquad = async (req, res) => {
   try {
     const { username, roomId } = req.body;
-    const response = await chatService.leaveChatRoom(username, roomId);
+    const response = await chatService.leaveSquad(username, roomId);
 
     if (response instanceof Error) {
       switch (response.message) {
         case "User not found":
           return Response.unauthorized(res, "User not found");
-        case "Room not found":
-          return Response.notFound(res, "Room not found");
-        case "User already left the room":
-          return Response.badRequest(res, "User already left the room");
+        case "Squad not found":
+          return Response.notFound(res, "Squad not found");
+        case "User already left the squad":
+          return Response.badRequest(res, "User already left the squad");
         case "Admin cannot leave as there are no other members to assign as admin":
           return Response.badRequest(res, "Admin cannot leave as there are no other members to assign as admin");
         default:
@@ -76,30 +76,30 @@ exports.leaveChatRoom = async (req, res) => {
       }
     }
 
-    return Response.success(res, "User has left room successfully", response);
+    return Response.success(res, "User has left squad successfully", response);
   } catch (error) {
-    console.error("Error in leaveChatRoom:", error.message);
+    console.error("Error in leaveSquad:", error.message);
     return Response.error(res, error.message || "Server error");
   }
 };
 
-exports.createRoom = async (req, res) => {
+exports.createSquad = async (req, res) => {
   try {
-    const { squadName, squadProfilePicture, description, adminName,adminUserName } = req.body;
-    const response = await chatService.createRoom(squadName, squadProfilePicture, description, adminName,adminUserName);
+    const { squadName, squadProfilePicture, description, adminName, adminUserName } = req.body;
+    const response = await chatService.createSquad(squadName, squadProfilePicture, description, adminName, adminUserName);
 
     if (response instanceof Error) {
       switch (response.message) {
-        case "Room is already created by this admin":
-          return Response.badRequest(res, "Room is already created by this admin");
+        case "Squad is already created by this admin":
+          return Response.badRequest(res, "Squad is already created by this admin");
         default:
           return Response.error(res, "Unexpected error occurred");
       }
     }
 
-   res.json({status:true,message:"Squad created successfully",data:response});
+    res.json({ status: true, message: "Squad created successfully", data: response });
   } catch (error) {
-    console.error("Error in createRoom:", error.message);
+    console.error("Error in createSquad:", error.message);
     return Response.badRequest(res, "Squad creation failed");
   }
 };
